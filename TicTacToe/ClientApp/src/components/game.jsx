5fbe4b34e701277d6fcaa601d5aa87dc8ca9bb84
kristoffer <zyprezz@gmail.com>
2020-01-18 20:45:15 +0100

    first commit

---
+++ TicTacToe/ClientApp/src/components/game.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+
+import Board from './Board'
+import Square from './Square'
+
+
+class game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
